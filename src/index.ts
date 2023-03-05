fad8b1db987e1e444938e1c86c2701c6adb806a7
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-05 17:49:22 +1100

    node2023: setup jest test

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,5 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+export default app;
---
+++ src/jest-test/server.spec.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import app from "../index";
+
+describe("Server", () => {
+  it("should return a 200 response when hitting the root URL", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+});
