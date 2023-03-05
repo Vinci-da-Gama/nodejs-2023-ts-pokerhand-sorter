5fe746ced6bce7fe3f68fa23c21d6df4962f5c14
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-05 22:48:51 +1100

    node2023: clear up

--- src/jest-test/server.spec.ts
+++ src/jest-test/server.spec.ts
@@ -2,8 +2,6 @@ import request from "supertest";
 // eslint-disable-next-line node/no-missing-import
 import { app } from "../index";
 
-import path from "path";
-
 describe("Server", () => {
   it("should return a 200 response when hitting the root URL", async () => {
     const response = await request(app).get("/");
