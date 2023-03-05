6d40e2d294199e603ce8a84b7747760e1b01be1c
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-05 22:23:41 +1100

    node2023: has error

--- src/jest-test/server.spec.ts
+++ src/jest-test/server.spec.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 // eslint-disable-next-line node/no-missing-import
-import app from "../index";
+import { app from "../index";
 
 describe("Server", () => {
   it("should return a 200 response when hitting the root URL", async () => {
